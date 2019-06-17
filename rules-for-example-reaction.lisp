adf5652a2b3ec9476cd5610dcacfba28bb7a9a36
bkane2 <Ben.kane@me.com>
2019-06-17 15:49:18 -0400

    Changed reaction to echo gist clause

--- rules-for-example-reaction.lisp
+++ rules-for-example-reaction.lisp
@@ -3,7 +3,9 @@
   '())
 
 (READRULES '*reaction-to-example-input*
-  '(1 (0 GIST \: 0)
-      2 (I said \: \' 4 \' \.) (0 :out)
+  '(1 (0 NIL GIST \: 0)
+      2 (Sorry \, you did not catch what I said \.) (0 :out)
+    1 (0)
+      2 (The gist of my reply is \: \' 1 \' \.) (0 :out)
   ))
 ); end of eval-when
