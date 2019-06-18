a7ec2cecf9cf6945bc6b87948000988da0d14100
bkane2 <Ben.kane@me.com>
2019-06-18 12:19:13 -0400

    Added ulf test cases

--- test.lisp
+++ test.lisp
@@ -1,7 +1,8 @@
 ;; File for testing example rule files
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(load "ttt/src/load"); needed: lissa6.lisp references ttt:...
+;; Load ttt and core Lissa functions
+(load "ttt/src/load")
 (defparameter *use-latency* nil)
 (defparameter *count* nil)
 (load "core/lissa6.lisp")
@@ -9,6 +10,8 @@
 (load "core/eval-lexical-ulfs.lisp")
 (load "core/stem.lisp")
 
+;; Prompt the user if they're running gist tests or ulf tests, and whether
+;; they want verbose printing
 (defparameter *gist?* nil)
 (defparameter *verbose* nil)
 (format t "~%~%~%~%~%~%~%~%~%")
@@ -17,6 +20,7 @@
 (princ "verbose (t or nil) ?: ") (finish-output)
 (setq *verbose* (if (equalp (write-to-string (read)) "t") t nil))
 
+;; Load rule file depending on gist or ulf
 (load (format nil "rules-input-~a.lisp" (if *gist?* "gist" "ulf")))
 
 
@@ -36,11 +40,12 @@
 ; Test whether the ulf extracted for a
 ; given query is equivalent to a given answer
 ;
-  (equalp answer
-    (form-ulf-from-clause query)))
+  (let ((ulf (form-ulf-from-clause query)))
+    (if *verbose* (format t "> The extracted ulf is:~%: ~a~%> The correct ulf is:~%: ~a~%" ulf answer))
+    (equalp answer ulf)))
 
 
-(defun run-tests (tests gist?)
+(defun run-tests (tests)
 ;``````````````````````````````
 ; Runs a list of tests ((query answer) pairs),
 ; returns the number of tests where the correct pattern
@@ -51,7 +56,7 @@
     (dolist (ti tests)
       (if *verbose*
         (format t "Testing: ~a~%``````````````````````````````````````~%" ti))
-      (if (if gist? (test-gist (first ti) (third ti))
+      (if (if *gist?* (test-gist (first ti) (third ti))
                     (test-ulf (first ti) (third ti)))
         (setf i (1+ i))
         (format t "Test failed: ~a~%" (first ti))))
@@ -71,28 +76,31 @@
   ; Gist tests
   ; Theme: "How did you get here today?"
   ;
-    '(((I refuse to answer this question \.) => (NIL Gist \: nothing found \.))
-    ((I drove \.)                            => (I drove myself here today \.))
-    ((I took my own car \.)                  => (I drove myself here today \.))
-    ((I had a friend drive me here \.)       => (Someone drove me here today \.))
-    ((I drove here with my wife \.)          => (I drove here today with my wife \.))
-    ((I got a ride with my friend \.)        => (Someone drove me here today \.))
-    ((I actually own a car \.)               => (I drove myself here today \.))
-    ((I don\'t live very far away \,
-      so I walked \.)                        => (I walked here today \.))
-    ((The bus \.)                            => (I took the bus here today \.))
-    ((The bus wasn\'t running today \, so
-      I had a friend drive me \.)            => (Someone drove me here today \.))
-    ((I used the shuttle from
-      graduate housing \.)                   => (I took the bus here today \.))
-    ((I\'m living on campus \,
-      so I was already here \.)              => (I walked here today \.))
-    ((I\'m living off campus \,
-      so I took an uber to campus today \.)  => (Someone drove me here today \.))
-    ((I caught an uber with a friend \.)     => (Someone drove me here today \.))
-    ((My car wasn\'t working today\, so I
-      ended up taking the bus \.)            => (I took the bus here today \.)))
-  t))
+    '(
+      ((I refuse to answer this question \.) => (NIL Gist \: nothing found \.))
+      ((I drove \.)                            => (I drove myself here today \.))
+      ((I took my own car \.)                  => (I drove myself here today \.))
+      ((I had a friend drive me here \.)       => (Someone drove me here today \.))
+      ((I drove here with my wife \.)          => (I drove here today with my wife \.))
+      ((I got a ride with my friend \.)        => (Someone drove me here today \.))
+      ((I actually own a car \.)               => (I drove myself here today \.))
+      ((I don\'t live very far away \,
+        so I walked \.)                        => (I walked here today \.))
+      ((The bus \.)                            => (I took the bus here today \.))
+      ((The bus wasn\'t running today \, so
+        I had a friend drive me \.)            => (Someone drove me here today \.))
+      ;----------------------------------------------------------------------------;
+      ((I used the shuttle from
+        graduate housing \.)                   => (I took the bus here today \.))
+      ((I\'m living on campus \,
+        so I was already here \.)              => (I walked here today \.))
+      ((I\'m living off campus \,
+        so I took an uber to campus today \.)  => (Someone drove me here today \.))
+      ((I caught an uber with a friend \.)     => (Someone drove me here today \.))
+      ((My car wasn\'t working today\, so I
+        ended up taking the bus \.)            => (I took the bus here today \.))
+    )
+  ))
 
 
   (t (run-tests
@@ -101,20 +109,52 @@
   ; Theme: Blocksworld queries to ULF
   ;
     '(
-      (() ())
+      ((Is the Nvidia block on the
+        Mercedes block ?)                    => (((PRES BE.V) (THE.D (|NVidia| BLOCK.N))
+                                                  (ON.P (THE.D (|Mercedes| BLOCK.N)))) ?))
+      ((Is there a red block on the
+        Mercedes block ?)                    => (((PRES BE.V) THERE.PRO (A.D (RED.A BLOCK.N))
+                                                  (ON.P (THE.D (|Mercedes| BLOCK.N)))) ?))
+      ((Is there a red Nvidia block on top
+        of the Mercedes block ?)             => (((PRES BE.V) THERE.PRO (A.D (RED.A (|NVidia| BLOCK.N)))
+                                                  (ON_TOP_OF.P (THE.D (|Mercedes| BLOCK.N)))) ?))
+      ((Is the Mcdonalds block to the left
+        of a red block ?)                    => (((PRES BE.V) (THE.D (|McDonalds| BLOCK.N))
+                                                  (TO_THE_LEFT_OF.P (A.D (RED.A BLOCK.N)))) ?))
+      ((Is there a green block under the
+        blue block ?)                        => (((PRES BE.V) THERE.PRO (A.D (GREEN.A BLOCK.N))
+                                                  (UNDER.P (THE.D (BLUE.A BLOCK.N)))) ?))
+      ;----------------------------------------------------------------------------------------------;
+      ((Is there a green block above the
+        blue block ?)                        => (((PRES BE.V) THERE.PRO (A.D (GREEN.A BLOCK.N))
+                                                  (ABOVE.P (THE.D (BLUE.A BLOCK.N))))))
+      ((Is there a green block on top of
+        the other green block ?)             => (((PRES BE.V) THERE.PRO (A.D (GREEN.A BLOCK.N))
+                                                  (ON_TOP_OF.P (THE.D (OTHER.A (GREEN.A BLOCK.N))))) ?))
+      ((Is the Nvidia block on the table ?)  => (((PRES BE.V) (THE.D (|NVidia| BLOCK.N))
+                                                  (ON.P (THE.D TABLE.N))) ?))
+      ((Is there a green block between a red
+        block and the SRI block ?)           => (((PRES BE.V) (A.D (GREEN.A BLOCK.N)) (BETWEEN.P
+                                                  ((A.D (RED.A BLOCK.N)) AND.CC (THE.D (|SRI| BLOCK.N))))) ?))
+      ((Is the Starbucks block above two
+        blue blocks ?)                       => (((PRES BE.V) (THE.D (|Starbucks| BLOCK.N)) (ABOVE.P
+                                                  (TWO.D (BLUE.A (PLUR BLOCK.N))))) ?))
+      ((Is the block above the Texaco block
+        red ?)                               => (((PRES BE.V) (THE.D (N+PREDS BLOCK.N (ABOVE.P (THE.D (|Texaco|
+                                                  BLOCK.N))))) RED.A) ?))
+      ((Is the block above the Texaco block
+        a green block ?)                     => (((PRES BE.V) (THE.D (N+PREDS BLOCK.N (ABOVE.P (THE.D (|Texaco|
+                                                  BLOCK.N))))) (= (A.D (GREEN.A) (BLOCK.N)))) ?))
+      ((Is the block below the Toyota block
+        on top of a red block ?)             => (((PRES BE.V) (THE.D (N+PREDS BLOCK.N (BELOW.P (THE.D (|Toyota|
+                                                  BLOCK.N))))) (ON_TOP_OF.P (A.D (RED.A BLOCK.N)))) ?))
+      ((Is there a red block under the blue
+        block that is under the SRI block ?) => (((PRES BE.V) THERE.PRO (A.D (RED.A BLOCK.N)) (UNDER.P (THE.D
+                                                  (N+PREDS (BLUE.A BLOCK.N) (THAT.REL ((PRES BE.V) (UNDER.P
+                                                  (THE.D (|SRI| BLOCK.N))))))))) ?))
+      ((Is the uppermost green block on top
+        of the Starbucks block ?)            => (((PRES BE.V) (THE.D (UPPERMOST.A (GREEN.A BLOCK.N)))
+                                                  (ON_TOP_OF.P (THE.D (|Starbucks| BLOCK.N)))) ?))
     )
-  nil))
+  ))
 )
-
-;; tests:
-;; (setq clause '(Is the Nvidia block on the Mercedes block ?))
-;; (trace choose-result-for choose-result-for1)
-;; (trace instance) ; more detailed tracing
-;; (trace eval-lexical-ulfs); still more detailed
-;; (form-ulf-from-clause clause)
-;; (setq clause '(Is there a red block on the Mercedes block ?))
-;; (form-ulf-from-clause clause)
-;; (setq clause '(Is there a red Nvidia block on top of the Mercedes block ?))
-;; (form-ulf-from-clause clause)
-;; (setq clause '(is the Mcdonalds block to the left of a red block ?))
-
