fe116c23303fffe0c399f0de974e69fed38ba3a3
bkane2 <Ben.kane@me.com>
2019-06-18 21:58:37 -0400

    Removed thematic/unbidden/question trees in gist clause selection

--- core/choose-gist-clause-trees-for-input.lisp
+++ core/choose-gist-clause-trees-for-input.lisp
@@ -3,9 +3,6 @@
    (READRULES '*gist-clause-trees-for-input*
    '(
       1 (0) 
-         2 (*specific-answer-from-example-input* 
-            *unbidden-answer-from-example-input*
-            *thematic-answer-from-example-input*
-            *question-from-example-input*) (0 :subtrees)
+         2 (*specific-answer-from-example-input*) (0 :subtrees)
    ))
 ); end of eval-when
